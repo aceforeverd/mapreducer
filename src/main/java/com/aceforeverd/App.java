c2f21442975857fc29abfadb6723f0fe4e765bc5
aceforeverd <aceforever@protonmail.com>
2018-05-22 21:22:25 +0800

    first commit

---
+++ src/main/java/com/aceforeverd/App.java
@@ -0,0 +1,13 @@
+package com.aceforeverd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
