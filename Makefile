df436302e51fcc5d6029d7e7cc845eba6dcd3684
aceforeverd <aceforever@protonmail.com>
2018-06-03 04:14:12 +0800

    added iot application 1

--- Makefile
+++ Makefile
@@ -1,27 +1,46 @@
 
-src = src/App.java
+src = src
 out = output
-application = App
-input = /data/input/emp.txt
+input = /data/input
 output = /data/output
 
-compile:
-	javac -classpath ${HADOOP_CLASSPATH} -d $(out) $(src)
+YARN = $(HADOOP_HOME)/bin/yarn
 
-package: compile
-	jar -cvf App.jar -C $(out) .
+app-compile:
+	javac -classpath ${HADOOP_CLASSPATH} -d $(out)/app $(src)/App.java
 
-run: package prepare
-	hadoop jar App.jar $(application) $(input) $(output)
+app-package: app-compile
+	jar -cvf App.jar -C $(out)/app .
 
-prepare:
-	hdfs dfs -test -d $(output); \
+app-prepare:
+	hdfs dfs -test -d "${output}/app"; \
 	if [ $$? -eq 0 ]; then \
-		hdfs dfs -rm -r $(output); \
+		hdfs dfs -rm -r "${output}/app"; \
 	fi
 
-yarn: package prepare
-	yarn jar App.jar $(application) $(input) $(output)
+app-yarn: app-package app-prepare
+	$(YARN) jar App.jar App $(input)/emp.txt $(output)/app
+
+app-run: app-package app-prepare
+	hadoop jar App.jar App $(input)/emp.txt $(output)/app
+
+
+iot1-compile:
+	javac -classpath $(HADOOP_CLASSPATH) -d $(out)/iot1 $(src)/IOT1.java
+
+iot1-package: iot1-compile
+	jar -cvf IOT1.jar -C $(out)/iot1
+
+iot1-prepare:
+	hdfs dfs -test -d "${output}/iot1"; \
+	if [ $$? -eq 0 ] ; then \
+		hdfs dfs -rm -r "${output}/iot1"; \
+	fi
+
+iot1-yarn: iot1-package iot1-prepare
+	$(YARN) jar IOT1.jar IOT1 $(input)/device.txt $(input)/dvalues.txt $(output)/iot1
+
+compile: app-compile iot1-compile
 
 clean:
-	rm -rf $(out)/* App.jar
+	rm -rf $(out)/* *.jar
---
+++ src/IOT1.java
@@ -0,0 +1,109 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class IOT1 {
+    public static class DeviceMapper extends Mapper<Object, Text, IntWritable, Text> {
+        private String[] fields;
+        private int id;
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            fields = value.toString().split("\\s+");
+            if (fields.length < 3) {
+                return;
+            }
+            id = Integer.parseInt(fields[0].trim());
+            if (id <= 0 || id >= 1000) {
+                return;
+            }
+
+            /* id + device + type */
+            context.write(new IntWritable(id), new Text("device " + fields[1].trim()));
+        }
+    }
+
+    public static class DValueMapper extends Mapper<Object, Text, IntWritable, Text> {
+        private String[] fields;
+        private int did;
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            fields = value.toString().split("\\s+");
+
+            if (fields.length < 3) {
+                return;
+            }
+
+            did = Integer.parseInt(fields[0].trim());
+            if (did <= 0 || did >= 1000) {
+                return;
+            }
+
+            if (!fields[1].trim().equals("\\N")) {
+                return;
+            }
+
+            /* did - value - date */
+            context.write(new IntWritable(did), new Text("value " + fields[1].trim()));
+        }
+    }
+
+    public static class JoinReducer extends Reducer<IntWritable, Text, Text, DoubleWritable> {
+        private TreeMap<String, Double> maps = new TreeMap<>();
+
+        public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            String type = "unknown";
+            double sumValue = 0.0;
+            String[] fields;
+
+            for (Text val : values) {
+                fields = val.toString().split("\\s+");
+                if (fields.length < 2) {
+                    /* not valid, ignore */
+                    continue;
+                }
+
+                if (fields[0].trim().equals("device")) {
+                    type = fields[1].trim();
+                } else if (fields[0].trim().equals("value")) {
+                    sumValue += Double.parseDouble(fields[1].trim());
+                }
+            }
+
+            maps.put(type, sumValue);
+        }
+
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            for (Map.Entry<String, Double> entry : maps.descendingMap().entrySet()) {
+                context.write(new Text(entry.getKey()), new DoubleWritable(entry.getValue()));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "IOT1");
+        job.setJarByClass(IOT1.class);
+
+        job.setReducerClass(JoinReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, DeviceMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, DValueMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
